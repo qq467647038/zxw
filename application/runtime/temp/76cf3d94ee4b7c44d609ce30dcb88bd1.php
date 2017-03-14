ccc32325075e8a6b39d378f3bfebf4406769fa4e
qq3341198392 <467647038@qq.com>
2017-03-14 10:07:12 +0800

    1

---
+++ application/runtime/temp/76cf3d94ee4b7c44d609ce30dcb88bd1.php
@@ -0,0 +1,29 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:65:"D:\phpStudy\WWW\zxw/application/admin/index\view\render\left.html";i:1489457102;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>这是左内容显示区域</title>
+</head>
+<body>
+	<div>
+		<div>
+			<div>
+				<img src="aside_img.png" />
+			</div>
+			<div>
+				<aside>
+					<ol>
+						<li>统计</li>
+						<li>计划</li>
+					</ol>
+					<ol>
+						<li>装修样式</li>
+						<li>装修厘米</li>
+					</ol>
+				</aside>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
